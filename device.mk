e65ad184648d0e2a7e3e5c3e5c5069a1650f494a
Jaekyun Seok <jaekyun@google.com>
2022-11-05 00:18:46 +0100

    a70q: Enforce RROs for all build-time overlays
    Change-Id: I2cf4bff42cbb2a48e2cd1e7b20a799350c1b467e

--- device.mk
+++ device.mk
@@ -251,6 +251,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/libnfc-qrd-SN100.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-qrd-SN100.conf \
     $(DEVICE_PATH)/rootdir/vendor/etc/libnfc-qrd-SN100_38_4MHZ.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-qrd-SN100_38_4MHZ.conf \
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Performance
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/perf/commonresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonresourceconfigs.xml \
